0c164557257c5fc2c92530bfe3c6c9f40fc28035
Nico-dF2209 <77271210+Nico-dF2209@users.noreply.github.com>
2023-06-22 21:34:53 +0200

    Big change 1

---
+++ Assets/Scripts/scr.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class scr : MonoBehaviour
+{
+    public void Playgame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+   
+    public void QuitGame()
+    {
+        Debug.Log("Quitting");
+
+        Application.Quit();
+    }
+}
